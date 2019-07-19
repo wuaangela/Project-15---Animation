24d1e1d2e1a83858a7ebfa971358c4074226b3f6
A W <waq.mmhy@live.ca>
2019-07-18 23:28:08 -0400

    Initial Commit

---
+++ Project 15 - Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 15 - Animation
+//
+//  Created by Angela Wu  on 2019-07-18.
+//  Copyright © 2019 Angela Wu . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
